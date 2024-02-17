ba18d495db28f7b8e3f6014e7d5faec04862088e
Andy Walter <musictopia2@hotmail.com>
2024-02-17 16:51:58 -0700

    Fixed a bug caused from last update for savannah.

--- AllMultiplayerGames.WebAssembly/Program.cs
+++ AllMultiplayerGames.WebAssembly/Program.cs
@@ -5,7 +5,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 bb1.OS = bb1.EnumOS.Wasm;
 ss1.IsWasm = true;
 //start
-GlobalClass.Version = "8.5";
+GlobalClass.Version = "8.6";
 //end
 LoaderGlobalClass.LoadSettingsAsync = GlobalSettingsExtensions.LoadGlobalDataAsync;
 LoaderGlobalClass.SaveSettingsAsync = GlobalSettingsExtensions.SaveGlobalDataAsync;
