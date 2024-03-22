bae720a65f283c7d721666661acbb3a09c28ca16
Andy Walter <musictopia2@hotmail.com>
2024-03-22 16:11:12 -0700

    Fixed another bug on golf card game. Also, made some minor balance changes for monopoly diced game.

--- AllMultiplayerGames.WebAssembly/Program.cs
+++ AllMultiplayerGames.WebAssembly/Program.cs
@@ -5,7 +5,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 bb1.OS = bb1.EnumOS.Wasm;
 ss1.IsWasm = true;
 //start
-GlobalClass.Version = "8.17";
+GlobalClass.Version = "8.18";
 //end
 LoaderGlobalClass.LoadSettingsAsync = GlobalSettingsExtensions.LoadGlobalDataAsync;
 LoaderGlobalClass.SaveSettingsAsync = GlobalSettingsExtensions.SaveGlobalDataAsync;
