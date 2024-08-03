e72b8c3639fae7e9069b3f5202c7f844a9c5a16e
Andy Walter <musictopia2@hotmail.com>
2024-08-03 09:55:42 -0700

    Fixed bug in hose hearts where the wrong player sometimes got the points from the blind.

--- AllMultiplayerGames.WebAssembly/Program.cs
+++ AllMultiplayerGames.WebAssembly/Program.cs
@@ -5,7 +5,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 bb1.OS = bb1.EnumOS.Wasm;
 ss1.IsWasm = true;
 //start
-GlobalClass.Version = "8.25";
+GlobalClass.Version = "8.26";
 //end
 LoaderGlobalClass.LoadSettingsAsync = GlobalSettingsExtensions.LoadGlobalDataAsync;
 LoaderGlobalClass.SaveSettingsAsync = GlobalSettingsExtensions.SaveGlobalDataAsync;
