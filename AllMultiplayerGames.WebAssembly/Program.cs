d58fd1f6db30e1afffd5b638a9faceb332995a97
Andy Walter <musictopia2@hotmail.com>
2024-12-23 09:59:22 -0700

    Recompiled all games and fixed breaking changes with IGenerateDice

--- AllMultiplayerGames.WebAssembly/Program.cs
+++ AllMultiplayerGames.WebAssembly/Program.cs
@@ -5,7 +5,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 bb1.OS = bb1.EnumOS.Wasm;
 ss1.IsWasm = true;
 //start
-GlobalClass.Version = "9.0";
+GlobalClass.Version = "9.1";
 //end
 LoaderGlobalClass.LoadSettingsAsync = GlobalSettingsExtensions.LoadGlobalDataAsync;
 LoaderGlobalClass.SaveSettingsAsync = GlobalSettingsExtensions.SaveGlobalDataAsync;
