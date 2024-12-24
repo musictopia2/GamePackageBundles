da5c6e2f355b417ea000055bc89cb58d2c5673de
Andy Walter <musictopia2@hotmail.com>
2024-12-24 13:27:28 -0700

    Updated game package to version 9.2

--- AllMultiplayerGames.WebAssembly/wwwroot/service-worker.published.js
+++ AllMultiplayerGames.WebAssembly/wwwroot/service-worker.published.js
@@ -6,7 +6,7 @@ self.addEventListener('install', event => event.waitUntil(onInstall(event)));
 self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
 self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
 //start
-const version = 9.1;
+const version = 9.2;
 //end
 const cacheNamePrefix = 'offline-cache-';
 const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
