3721bbf70cfbc3e0436d3b3c68548c9763bad880
Andy Walter <musictopia2@hotmail.com>
2025-05-15 20:34:46 -0700

    Updated game package to version 9.24

--- AllMultiplayerGames.WebAssembly/Program.cs
+++ AllMultiplayerGames.WebAssembly/Program.cs
@@ -5,7 +5,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 bb1.OS = bb1.EnumOS.Wasm;
 ss1.IsWasm = true;
 //start
-GlobalClass.Version = "9.23";
+GlobalClass.Version = "9.24";
 //end
 LoaderGlobalClass.LoadSettingsAsync = GlobalSettingsExtensions.LoadGlobalDataAsync;
 LoaderGlobalClass.SaveSettingsAsync = GlobalSettingsExtensions.SaveGlobalDataAsync;
