d1fb16e8bff9f5cdbd310ec7d691230d86951d8f
Andy Walter <musictopia2@hotmail.com>
2025-07-12 09:50:32 -0700

    Updated basic packages on 7/12/2025 9:50:32 AM

--- AllMultiplayerGames.WebAssembly/wwwroot/service-worker.published.js
+++ AllMultiplayerGames.WebAssembly/wwwroot/service-worker.published.js
@@ -1,51 +1,56 @@
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
-
 self.importScripts('./service-worker-assets.js');
-self.addEventListener('install', event => event.waitUntil(onInstall(event)));
-self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
-self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
+
 //start
-const version = 9.33;
+const version = 9.34;
 //end
+
 const cacheNamePrefix = 'offline-cache-';
 const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
 const offlineAssetsInclude = [/\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/];
 const offlineAssetsExclude = [/^service-worker\.js$/];
 
-async function onInstall(event) {
+self.addEventListener('install', event => {
     console.info('Service worker: Install');
-
-    // Fetch and cache all matching items from the assets manifest
-    const assetsRequests = self.assetsManifest.assets
-        .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
-        .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
-        .map(asset => new Request(asset.url, { integrity: asset.hash }));
-    self.skipWaiting();
-    await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
-}
-
-async function onActivate(event) {
-    console.info('Service worker: Activate');
-
-    // Delete unused caches
-    const cacheKeys = await caches.keys();
-    await Promise.all(cacheKeys
-        .filter(key => key.startsWith(cacheNamePrefix) && key !== cacheName)
-        .map(key => caches.delete(key)));
-}
-
-async function onFetch(event) {
-    let cachedResponse = null;
-    if (event.request.method === 'GET') {
-        // For all navigation requests, try to serve index.html from cache
-        // If you need some URLs to be server-rendered, edit the following check to exclude those URLs
-        const shouldServeIndexHtml = event.request.mode === 'navigate';
-
-        const request = shouldServeIndexHtml ? 'index.html' : event.request;
+    event.waitUntil((async () => {
+        const assetsRequests = self.assetsManifest.assets
+            .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
+            .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
+            .map(asset => new Request(asset.url, { integrity: asset.hash }));
         const cache = await caches.open(cacheName);
-        cachedResponse = await cache.match(request);
-    }
+        await cache.addAll(assetsRequests);
+        await self.skipWaiting();
+    })());
+});
 
-    return cachedResponse || fetch(event.request);
-}
+self.addEventListener('activate', event => {
+    console.info('Service worker: Activate');
+    event.waitUntil((async () => {
+        const cacheKeys = await caches.keys();
+        await Promise.all(
+            cacheKeys
+                .filter(key => key.startsWith(cacheNamePrefix) && key !== cacheName)
+                .map(key => caches.delete(key))
+        );
+        await self.clients.claim();
+    })());
+});
+
+self.addEventListener('fetch', event => {
+    if (event.request.method !== 'GET') return;
+
+    event.respondWith((async () => {
+        const cache = await caches.open(cacheName);
+        const cachedResponse = await cache.match(event.request);
+
+        const networkFetch = fetch(event.request).then(response => {
+            if (response && response.status === 200) {
+                cache.put(event.request, response.clone());
+            }
+            return response;
+        }).catch(() => cachedResponse); // fallback to cache if network fails
+
+        return cachedResponse || networkFetch;
+    })());
+});
