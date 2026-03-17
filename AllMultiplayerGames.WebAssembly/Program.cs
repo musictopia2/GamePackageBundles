3d2fb724a05ba59ed401ed518bff0b8b01905ee1
Andy Walter <musictopia2@hotmail.com>
2026-03-17 08:56:26 -0700

    Updated game package to version 10.7

--- AllMultiplayerGames.WebAssembly/Program.cs
+++ AllMultiplayerGames.WebAssembly/Program.cs
@@ -7,7 +7,7 @@ ss1.IsWasm = true;
 aa2.AppStyleName = "AllMultiplayerGames.WebAssembly";
 aa2.AdditionalAssembly = typeof(LoaderViewModel).Assembly;
 //start
-GlobalClass.Version = "10.6";
+GlobalClass.Version = "10.7";
 //end
 LoaderGlobalClass.LoadSettingsAsync = GlobalSettingsExtensions.LoadGlobalDataAsync;
 LoaderGlobalClass.SaveSettingsAsync = GlobalSettingsExtensions.SaveGlobalDataAsync;
