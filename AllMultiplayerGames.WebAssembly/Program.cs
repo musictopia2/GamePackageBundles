10095c81f4b6d6c94b965cc26a80eb0af1caff1d
Andy Walter <musictopia2@hotmail.com>
2026-03-16 21:44:46 -0700

    Updated basic packages on 3/16/2026 9:44:45 PM

--- AllMultiplayerGames.WebAssembly/Program.cs
+++ AllMultiplayerGames.WebAssembly/Program.cs
@@ -7,7 +7,7 @@ ss1.IsWasm = true;
 aa2.AppStyleName = "AllMultiplayerGames.WebAssembly";
 aa2.AdditionalAssembly = typeof(LoaderViewModel).Assembly;
 //start
-GlobalClass.Version = "10.5";
+GlobalClass.Version = "10.6";
 //end
 LoaderGlobalClass.LoadSettingsAsync = GlobalSettingsExtensions.LoadGlobalDataAsync;
 LoaderGlobalClass.SaveSettingsAsync = GlobalSettingsExtensions.SaveGlobalDataAsync;
--- AllMultiplayerGames.WebAssembly/wwwroot/service-worker.published.js
+++ AllMultiplayerGames.WebAssembly/wwwroot/service-worker.published.js
@@ -3,7 +3,7 @@
 self.importScripts('./service-worker-assets.js');
 
 //start
-const version = 10.5;
+const version = 10.6;
 //end
 
 const cacheNamePrefix = 'offline-cache-';
